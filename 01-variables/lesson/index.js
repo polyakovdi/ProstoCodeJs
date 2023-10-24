07c9a45f3ca1c294f8253317b8a70c7cd2054055
polyakovdi <goodboy393393@gmail.com>
2023-10-25 00:14:57 +0300

    add lesson , homework and index.js

---
+++ 01-variables/lesson/index.js
@@ -0,0 +1,14 @@
+let name = "Dmitry";
+let age = 36;
+let boolean = true;
+let number = 777;
+let string = "ProstoCode";
+let value = null;
+let valueOther = undefined;
+
+let city = {};
+
+const welcome = function () {
+    console.log("Hello, world!")
+}
+welcome();
